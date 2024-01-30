6a9b1ac32865a7dcea5eff625f446282ca3c0bcc
nedoratic <nedoratic@gmail.com>
2024-01-30 22:44:41 +0100

    fetching JSON data

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
+// URL variable
 let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
+// Request variable
 let req = new XMLHttpRequest();
 
+// Data variable
 let data;
+let values = [];
 
-let values;
-
+// Bar Chart variables
 let xAxis;
 let yAxis;
-
 let xScale;
 let yScale;
-
 let width = 1000;
 let height = 600;
 let margin = 50;
 
+// SVG
 let svg = d3.select("svg");
 
+// Draw Bar Chart
 let drawBarChart = () => {
 	svg.attr("width", width);
 	svg.attr("height", height);
 };
 
-let generateAxes = () => {};
+let drawAxes = () => {};
 
-let generateScales = () => {};
+let drawScales = () => {};
 
 let drawBars = () => {};
+
+// Fetching JSON data
+req.open("GET", url, true);
+req.onload = () => {
+	data = JSON.parse(req.responseText);
+	values = data.data;
+	console.log(values);
+	drawBarChart();
+	drawAxes();
+	drawScales();
+	drawBars();
+};
+req.send();
