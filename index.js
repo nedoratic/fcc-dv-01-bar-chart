b63b0d3e0050166b0f6b2aa68678a4116cf9e4f1
nedoratic <nedoratic@gmail.com>
2024-01-30 22:26:41 +0100

    basic variables added

---
+++ index.js
@@ -0,0 +1,17 @@
+let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+
+let req = new XMLHttpRequest();
+
+let data;
+
+let values;
+
+let xAxis;
+let yAxis;
+
+let xScale;
+let yScale;
+
+let width = 1000;
+let height = 600;
+let margin = 50;
