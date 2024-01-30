8817930057762c844b7046d1f34563b98621d9fa
nedoratic <nedoratic@gmail.com>
2024-01-30 22:27:24 +0100

    basic functions added

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ let yScale;
 let width = 1000;
 let height = 600;
 let margin = 50;
+
+let svg = d3.select("svg");
+
+let drawBarChart = () => {
+	svg.attr("width", width);
+	svg.attr("height", height);
+};
+
+let generateAxes = () => {};
+
+let generateScales = () => {};
+
+let drawBars = () => {};
