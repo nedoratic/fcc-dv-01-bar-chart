2f657f8246f2201eb85a6b9bacee3dae4ac8784f
nedoratic <nedoratic@gmail.com>
2024-02-01 22:42:39 +0100

    draw bars function added

--- index.js
+++ index.js
@@ -1,58 +1,112 @@
-// URL variable
+// URL Variable
 let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-// Request variable
+// Request Variable
 let req = new XMLHttpRequest();
 
-// Data variable
+// Data Variable
 let data;
 let values = [];
 
-// Bar Chart variables
-let xAxis;
-let yAxis;
+// Bar Chart Variables
 let xScale;
+let xAxisScale;
+
 let yScale;
-let width = 1000;
+let yAxisScale;
+
+let width = 800;
 let height = 600;
 let padding = 50;
 
-// SVG
+// SVG Variable
 let svg = d3.select("svg");
 
-// Draw Bar Chart
-let drawBarChart = () => {
+// Draw Chart
+let drawChart = () => {
 	svg.attr("width", width);
 	svg.attr("height", height);
 };
 
+// Draw Scales
 let drawScales = () => {
-	xAxis = d3
+	yScale = d3
 		.scaleLinear()
-		.domain([0, d3.max(values, (gdp) => gdp[1])])
+		.domain([
+			0,
+			d3.max(values, (item) => {
+				return item[1];
+			}),
+		])
 		.range([0, height - 2 * padding]);
 
-	yAxis = d3
+	xScale = d3
 		.scaleLinear()
 		.domain([0, values.length - 1])
 		.range([padding, width - padding]);
 
-	let datesArray = values.map((date) => new Date(date[0]));
+	let datesArray = values.map((item) => {
+		return new Date(item[0]);
+	});
 
-	xScale = d3
+	xAxisScale = d3
 		.scaleTime()
 		.domain([d3.min(datesArray), d3.max(datesArray)])
 		.range([padding, width - padding]);
 
-	yScale = d3
+	yAxisScale = d3
 		.scaleLinear()
-		.domain([0, d3.max(values, (item) => item[1])])
+		.domain([
+			0,
+			d3.max(values, (item) => {
+				return item[1];
+			}),
+		])
 		.range([height - padding, padding]);
 };
 
+// Draw Bars
+let drawBars = () => {
+	let tooltip = d3.select("body").append("div").attr("id", "tooltip").style("visibility", "hidden").style("width", "auto").style("height", "auto");
+
+	svg
+		.selectAll("rect")
+		.data(values)
+		.enter()
+		.append("rect")
+		.attr("class", "bar")
+		.attr("width", (width - 2 * padding) / values.length)
+		.attr("data-date", (item) => {
+			return item[0];
+		})
+		.attr("data-gdp", (item) => {
+			return item[1];
+		})
+		.attr("height", (item) => {
+			return yScale(item[1]);
+		})
+		.attr("x", (item, index) => {
+			return xScale(index);
+		})
+		.attr("y", (item) => {
+			return height - padding - yScale(item[1]);
+		})
+		.on("mouseover", (item) => {
+			tooltip.transition().style("visibility", "visible");
+
+			tooltip.text(item[0]);
+
+			document.querySelector("#tooltip").setAttribute("data-date", item[0]);
+		})
+		.on("mouseout", (item) => {
+			tooltip.transition().style("visibility", "hidden");
+		});
+};
+
+// Draw Axes
 let drawAxes = () => {
-	let xAxis = d3.axisBottom(xScale);
-	let yAxis = d3.axisLeft(yScale);
+	let xAxis = d3.axisBottom(xAxisScale);
+	let yAxis = d3.axisLeft(yAxisScale);
 
 	svg
 		.append("g")
@@ -67,17 +121,17 @@ let drawAxes = () => {
 		.attr("transform", "translate(" + padding + ", 0)");
 };
 
-let drawBars = () => {};
-
-// Fetching JSON data
+// Fetch JSON Data
 req.open("GET", url, true);
+
 req.onload = () => {
 	data = JSON.parse(req.responseText);
 	values = data.data;
 	console.log(values);
-	drawBarChart();
+	drawChart();
 	drawScales();
-	drawAxes();
 	drawBars();
+	drawAxes();
 };
+
 req.send();
